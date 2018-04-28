2c23411d5b71aae7fe1c9578d9d55b09a8ac7cba
Johnny <157781745@qq.com>
2018-04-28 23:24:06 +0800

    update
     fix bugs

--- src/modules/Toolbar.js
+++ src/modules/Toolbar.js
@@ -3,12 +3,21 @@ import IconAlignCenter from 'quill/assets/icons/align-center.svg';
 import IconAlignRight from 'quill/assets/icons/align-right.svg';
 import { BaseModule } from './BaseModule';
 
-const Parchment = window.Quill.imports.parchment;
-const FloatStyle = new Parchment.Attributor.Style('float', 'float');
-const MarginStyle = new Parchment.Attributor.Style('margin', 'margin');
-const DisplayStyle = new Parchment.Attributor.Style('display', 'display');
+
+let FloatStyle
+let MarginStyle
+let DisplayStyle
+let Quill;
 
 export class Toolbar extends BaseModule {
+	constructor (resizer) {
+		super(resizer);
+		Quill=resizer.quill.constructor;
+		let Parchment = Quill.imports.parchment;
+		 FloatStyle = new Parchment.Attributor.Style('float', 'float');
+		 MarginStyle = new Parchment.Attributor.Style('margin', 'margin');
+		 DisplayStyle = new Parchment.Attributor.Style('display', 'display');
+	};
     onCreate = () => {
 		// Setup Toolbar
         this.toolbar = document.createElement('div');
